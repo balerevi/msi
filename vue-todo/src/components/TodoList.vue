1dd3f30c6fa77749e47d0d9574a88f35def40bbe
eh <evelyn.holtkamp@gmail.com>
2020-04-06 19:44:17 +0200

    Erste Implementierung Vue-TODO-List

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="todo">
+    <input type="text" placeholder="Neuer Task" v-model="neuerTask" @keyup.enter="addTask"/>
+
+    <div v-for="(task, index) in tasks" :key="task.id">
+        <input type="checkbox" v-model="task.erledigt"/>
+        {{task.title}}
+      <a @click="removeTask(index)" >
+       &times;
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      neuerTask: '',
+      tasks:[
+        {
+          'id':1,
+          'title':'Task 1',
+          'erledigt': false
+        },
+        {
+          'id':2,
+          'title':'Task 2',
+          'erledigt': false
+        }
+      ],
+      taskId: 3
+    }
+  },
+  methods:{
+    addTask(){
+      if (this.neuerTask.trim().length === 0){
+        return;
+      }
+      this.tasks.push({
+        id:this.taskId,
+        title:this.neuerTask,
+        erledigt: false
+      }),
+      this.neuerTask='',
+      this.taskId++
+    },
+    removeTask(index){
+      this.tasks.splice(index,1);
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
