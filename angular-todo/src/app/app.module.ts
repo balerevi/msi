0c26a24af10be2aa63e6ac4d186a6a84bce4c9cd
Helena Michaelsen R <helenamr@gmail.com>
2020-04-07 18:55:10 +0200

    First Angular ToDo List implementation
    Checking/unckecking, adding and removing tasks, loosely following the vue.js implementation

--- angular-todo/src/app/app.module.ts
+++ angular-todo/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
