ed0dc1e9fa29ce1d31719df597c4e04432fe71d9
Helena Michaelsen R <helenamr@gmail.com>
2020-05-07 14:15:43 +0200

    missing react file

---
+++ react-todo/src/TodoList.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newTask: '',
+            tasks:[
+                {
+                    'id':1,
+                    'title':'Task 1',
+                    'completed': true
+                },
+                {
+                    'id':2,
+                    'title':'Task 2',
+                    'completed': true
+                },
+                {
+                    'id':3,
+                    'title':'Task 3',
+                    'completed': false
+                }
+            ]
+        };
+        this.taskId = this.getTail(this.state.tasks).id;
+    }
+    render() {
+        return (
+            <div className="todo">
+                <input type="text" placeholder="New Task" value={this.state.newTask}
+                       onChange={e => this.setState({ "newTask": e.target.value})}/>
+                <button onClick={() => this.addTask()}>Add</button>
+                <ul>
+                    {this.state.tasks.map((task, index) => {
+                        return (
+                            <p>
+                                <input type="checkbox" checked={task.completed} onClick={() => this.toggleTaskStatus(index)} />
+                                {task.title}
+                                <span className="red" onClick={() => this.removeTask(index)}> X</span>
+                            </p>
+                        )
+                    })}
+                </ul>
+            </div>
+        );
+    }
+    addTask(){
+        this.taskId += 1;
+        let updatedList = this.state.tasks;
+        let newTask = {
+            id:this.taskId,
+            title:this.state.newTask,
+            completed: false
+        };
+
+        if (newTask.title.trim().length === 0){
+            newTask.title = 'Task ' + this.taskId;
+        }
+
+        updatedList.push(newTask);
+        this.setState({ "newTask": '', "tasks": updatedList});
+    }
+    removeTask(index){
+        let updatedList = this.state.tasks;
+        updatedList.splice(index,1);
+        this.setState({ "tasks": updatedList});
+    }
+    toggleTaskStatus(index){
+        let updatedList = this.state.tasks;
+        updatedList[index].completed = !updatedList[index].completed;
+        this.setState({ "tasks": updatedList});
+    }
+    getLastIndex(array){
+        return array.length-1;
+    }
+    getTail(array){
+        return array[this.getLastIndex(array)];
+    }
+}
+
+export default TodoList;
